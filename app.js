665e9831f15e2f8bf2581dde7daee2895ccb7ae0
geekahmed <geekahmed1@gmail.com>
2020-08-27 21:54:14 +0200

    Adding Some Important Packages

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+require('dotenv').config()
+const mongoose = require('mongoose');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+mongoose.connect(process.env.MONGO_ATLAS_URL,{useNewUrlParser: true}, () => {console.log("Connected to the database");});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
